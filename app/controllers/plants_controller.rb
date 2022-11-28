04f251be38726009997129174f8ffaca52e83efc
Christian <cedenoc181@gmail.com>
2022-11-28 00:23:43 -0500

    commit tree 673b0115fb99514b9a4e1c8bd92ad6a74409f5c4

--- app/controllers/plants_controller.rb
+++ app/controllers/plants_controller.rb
@@ -8,7 +8,7 @@ class PlantsController < ApplicationController
 
   # GET /plants/:id
   def show
-    plant = Plant.find_by(id: params[:id])
+    plant = find_plant
     render json: plant
   end
 
@@ -17,10 +17,33 @@ class PlantsController < ApplicationController
     plant = Plant.create(plant_params)
     render json: plant, status: :created
   end
+  
+  def update
+    plant = find_plant
+    if plant 
+      plant.update(plant_params)
+      render json: plant
+    else 
+      render json: { error: "plant not found" }, status: :not_found 
+    end 
+  end
 
+  def destroy
+    plant = find_plant
+    if plant 
+      plant.destroy
+      head :no_content
+    else 
+      render json: { error: "plant not found" }, status: :not_found 
+    end
+  end
   private
 
   def plant_params
     params.permit(:name, :image, :price, :is_in_stock)
+  end
+
+  def find_plant
+    plant = Plant.find_by(id: params[:id])
   end
 end
